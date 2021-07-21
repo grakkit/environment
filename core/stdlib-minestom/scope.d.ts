1811737780be6cf2400af90819c05b6d2adf4f7b
hb432 <hb432@protonmail.com>
2021-07-21 18:47:25 -0400

    when the types are STILL sus

--- core/stdlib-minestom/scope.d.ts
+++ core/stdlib-minestom/scope.d.ts
@@ -1,4 +1,4 @@
-export declare const core: typeof import('./index');
-export declare const extension: import('@grakkit/types-minestom').nmseExtension;
-export declare const manager: import('@grakkit/types-minestom').nmseExtensionManager;
-export declare const server: import('@grakkit/types-minestom').nmsMinecraftServer;
+declare const core: typeof import('./index');
+declare const extension: import('@grakkit/types-minestom').nmseExtension;
+declare const manager: import('@grakkit/types-minestom').nmseExtensionManager;
+declare const server: import('@grakkit/types-minestom').nmsMinecraftServer;
